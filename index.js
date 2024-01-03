2034a24896dbf5c8ab4933986fbf0c0db7dfdd6c
Mikenson Thomas <thomasmikenson40@gmail.com>
2024-01-03 00:47:18 -0300

    Commit

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ function validate() {
         btnSalvar.disabled = false;
     }
 }
+
+function validar(){
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
+    const btnEntrar = document.getElementById("btnEntrar");
+
+    if(!email || !senha || !validateEmail(email)){
+        btnEntrar.disabled = true;
+    } else if(senha.length < 6){
+        alert("A senha deve ter pelo menos 6 caracteres");
+        btnEntrar.disabled = true;
+    } else {
+        btnEntrar.disabled = false;
+    }
+}
