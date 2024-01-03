8f75e7a0bfb6449ac79ece2f8253bc4e9a7aa708
Mikenson Thomas <thomasmikenson40@gmail.com>
2024-01-02 21:05:35 -0300

    Commit

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBWBaKi39cA7-8RlrQcGQ4aRvRWVHugtjo",
+    authDomain: "plataforma-online-tcc.firebaseapp.com",
+    projectId: "plataforma-online-tcc",
+    storageBucket: "plataforma-online-tcc.appspot.com",
+    messagingSenderId: "609402086727",
+    appId: "1:609402086727:web:fd3c428bd2650c3efbd862"
+};
+
+const app = initializeApp(firebaseConfig);
