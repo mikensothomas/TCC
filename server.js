66f93bdfe83b3be0cdf24e94a076c6ebc5ac3f94
Mikenson Thomas <thomasmikenson40@gmail.com>
2024-01-06 04:29:12 -0300

    Commit

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const http = require("http");
 
 const app = express();
 const server = http.createServer(app);
-const PORT = 4000;
+const PORT = 8080;
 app.use(express.static(path.join(__dirname, 'public')));
 server.listen(PORT, () => "Servidor online na porta " + PORT);
