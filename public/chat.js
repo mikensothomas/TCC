55dae3764083eb00b90ae20c125d2a7c56a36cb4
Mikenson Thomas <thomasmikenson40@gmail.com>
2024-01-13 22:00:24 -0300

    Commit

--- public/chat.js
+++ public/chat.js
@@ -1,11 +1,30 @@
 function entrar(){
-    const nome = document.getElementById("nome").value;
-    if(!nome){
+    const nomes = document.getElementById("nomes").value;
+    if(!nomes){
         alert("Por favor preencha o campo nome");
     } else {
+        const nomeArmazenado = JSON.parse(localStorage.getItem('nomes')) || [];
+        nomeArmazenado.push(nomes);
+        localStorage.setItem('nomes', JSON.stringify(nomeArmazenado));
+        document.getElementById("nomes").value = "";
+        atualizarListaDeNome();
         window.location.href = "chat.html";
     }
 }
+
+function atualizarListaDeNome(){
+    const nomesArmazenados = JSON.parse(localStorage.getItem('nomes')) || [];
+    const listaNome = document.getElementById("pessoa");
+    listaNome.innerHTML = "";
+
+    nomesArmazenados.forEach(function(nomes) {
+        const li = document.createElement('li');
+        li.textContent = nomes;
+        listaNome.appendChild(li);
+    });
+}
+
+atualizarListaDeNome();
 
 function digitar_mensagem() {
     const quadroMensagem = document.getElementById("quadro-mesnsagem");
